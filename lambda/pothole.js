90430fbd098dbe2b1e27b57388a6ec2e0056fca3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-05 03:30:11 +0000

    updated lambda/pothole.js.

---
+++ lambda/pothole.js
@@ -0,0 +1,23 @@
+const Alexa = require('ask-sdk-core');
+
+const PotHoleRequestHandler = {
+    canHandle(handlerInput){
+        return (
+            Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+            Alexa.getIntentName(handlerInput.requestEnvelope) === "PotHoleIntent"
+        )
+        },
+        handle(handlerInput){
+            const speechText =  'Thank you for reporting the pot hole.'
+            return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Thank you for reporting the pot hole.')
+            
+        
+        .getResponse()
+    },
+    
+}
+
+module.exports = { PotHoleRequestHandler }
